c9324f20174a3a7ef55cfdc946abc03a77494e4a
David Ramiro <manuel-david.ramiro@gfi.world>
2019-09-27 10:04:06 +0200

    end breakingbad project

--- breakingbad/src/App.js
+++ breakingbad/src/App.js
@@ -1,7 +1,29 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Frase from './components/Frase';
 
 function App() {
-  return <h1>BreakingBad</h1>;
+  const [frase, obtenerFrase] = useState({});
+
+  async function getFrase() {
+    const resultado = await axios(
+      'https://breaking-bad-quotes.herokuapp.com/v1/quotes'
+    );
+
+    if (resultado) {
+      obtenerFrase(resultado.data[0]);
+    }
+  }
+
+  useEffect(() => {
+    getFrase();
+  }, []);
+  return (
+    <div className='contenedor'>
+      <Frase frase={frase} />
+      <button onClick={getFrase}>Generar frase</button>
+    </div>
+  );
 }
 
 export default App;
