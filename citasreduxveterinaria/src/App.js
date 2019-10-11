6ce8e63a43d24af2e373fd601318521b955fa508
David Ramiro Barragan <dramirobarragan@gmail.com>
2019-10-11 13:39:30 +0200

    add citasreduxveterinariaproject

---
+++ citasreduxveterinaria/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import store from './store';
+import { Provider } from 'react-redux';
+import  AgregarCita  from './componentes/AgregarCita';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className='container'>
+        <header className='text-center'>
+          <h1>Administrador d epacientes de Veterinaria</h1>
+        </header>
+
+        <div className='row mt-3'>
+          <div className='col-md-6'>
+            <AgregarCita />
+          </div>
+          <div className='col-md-6'>Listado</div>
+        </div>
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
